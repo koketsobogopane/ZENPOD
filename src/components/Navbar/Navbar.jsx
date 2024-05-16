1bb9e6915c94a67987ab37f39ff30a718209b850
your_username <koketsobogopanenov24@gmail.com>
2024-05-16 23:27:48 +0200

    Add logo navigation to home path

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,56 +11,50 @@ import SearchSharpIcon from '@mui/icons-material/SearchSharp';
 import SearchModal from '../SearchModal/SearchModal';
 import { FormControl, Menu, MenuItem, Select } from '@mui/material';
 import styles from './Navbar.module.css';
-import Logo from '../../../public/assets/ZenLogo.png'
+import Logo from '../../../public/assets/ZenLogo.png';
+import { Link } from 'react-router-dom';
 
-
-const option = [
-    'A-Z',
-    'Z-A',
-    'Oldest to Newest',
-    'Newest to Oldest'
-]
+const option = ['A-Z', 'Z-A', 'Oldest to Newest', 'Newest to Oldest'];
 
 const Navbar = (props) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [anchorEl, setAnchorEl] = useState(null)
-    const [sortValue, setSortValue] = useState('A-Z')
-    
-    const open = Boolean(anchorEl)
-    const handleClick = (event) => {
-            setAnchorEl(event.currentTarget)
-    }
-    const handleCloseMenu = () => {
-        setAnchorEl(null);
-    }
+  const [isOpen, setIsOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [sortValue, setSortValue] = useState('A-Z');
 
-    const handleClickOpen = () => {
-        setIsOpen(true);
-      };
-    
-      const handleClose = () => {
-        setIsOpen(false);
-      };
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
 
-    const handleItemsClick = () => {
-        handleCloseMenu()
-    }
+  const handleClickOpen = () => {
+    setIsOpen(true);
+  };
 
-    const StyleAppBar = styled(AppBar)`
-        background-color: #79B791;
-        color: #51291E;
-    ` 
-    return (
-        <header className={styles.header}>
-            <div className = {styles.logoContainer}>
-            <img src={Logo} alt="logo" />
-            <h1>ZENPOD</h1>
-            </div>
-            
-            <SearchSharpIcon />
-        </header>
-    )
-}
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
+  const handleItemsClick = () => {
+    handleCloseMenu();
+  };
 
-export default Navbar
+  const StyleAppBar = styled(AppBar)`
+    background-color: #79b791;
+    color: #51291e;
+  `;
+  return (
+    <header className={styles.header}>
+      <Link to={'/'} className={styles.logoContainer}>
+        <img src={Logo} alt="logo" />
+        <h1>ZENPOD</h1>
+      </Link>
+
+      <SearchSharpIcon />
+    </header>
+  );
+};
+
+export default Navbar;
